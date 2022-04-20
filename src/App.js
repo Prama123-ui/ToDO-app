9e60aa4088441410cbbe185449cfd88fd24ba1cc
prama pandit <panditprama@gmail.com>
2022-04-20 16:56:58 +0545

    modify app

--- src/App.js
+++ src/App.js
@@ -1,61 +1,11 @@
-import './App.css';
-import Header from './Componenet/Header';
-import ToDos from './Componenet/ToDos';
-import Footer from './Componenet/Footer';
-import React, { useEffect, useState } from 'react';
-import AddTodo from './Componenet/AddTodo';
-import { cleanup } from '@testing-library/react';
-
-
+import React from 'react'
+import Todo from './Components/Todo';
 function App() {
-  let initTodo;
-  
-  if (localStorage.getItem("todos")===null){
-    initTodo = [];
-  }
-  else{
-    initTodo = JSON.parse(localStorage.getItem("todos"));
-  }
-  const onDelete = (todo)=>{
-    console.log("I am onDelete", todo);
-    
-    setToDos(todos.filter((e)=>{
-      return e!==todo;
-    }));
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
-
-  const addTodo = (title, desc)=>{
-    console.log("I am adding this todo", title, desc )
-    let sno;
-    if(todos.length==0){
-      sno = 0;
-    }
-    else{
-      sno = todos[todos.length-1].sno + 1;
-    }
-    const myTodo = {
-      sno: sno,
-      title: title,
-      desc: desc
-    }
-    setToDos([...todos,myTodo]);
-    console.log(myTodo);
-  }
-
-  const [todos, setToDos] = useState(initTodo);
-  useEffect(()=>{
-    localStorage.setItem("todos", JSON.stringify(todos));
-    
-  }, [todos])
-  return (
-    <>
-      <Header title="My Todos Project" searchBar={true} />
-      <AddTodo addTodo={addTodo}/>
-      <ToDos todos={todos} onDelete={onDelete}/>
-      <Footer />
-    </>
-  )
+    return (
+        <div>
+            <Todo/>
+        </div>
+    )
 }
 
 export default App;
